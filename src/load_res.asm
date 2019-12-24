9b006ce93f15babc7c639115ac37239572564d85
roya <roya@kniv.local>
2019-12-24 12:25:59 +0100

    Change song

--- src/load_res.asm
+++ src/load_res.asm
@@ -1,6 +1,6 @@
 ; load sid music
 
 * = address_music                  					; address to load the music data
-!bin "res/datatanten_strikes_back.sid",, $7c+2    	; remove header from sid and cut off
+!bin "res/no_name.sid",, $7c+2    	; remove header from sid and cut off
                                    					; original loading address
 
